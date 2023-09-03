e9d4b62675d7ebf8f8a3e4b4b41d37b1d930c6af
zw.ruan.sn <zw.ruan.sn@gmail.com>
2023-09-03 10:28:38 +0800

    Add: complete engine

--- README.md
+++ README.md
@@ -1,4 +1,26 @@
 # 💤 LazyVim
 
-A starter template for [LazyVim](https://github.com/LazyVim/LazyVim).
-Refer to the [documentation](https://lazyvim.github.io/installation) to get started.
+> A starter template for [LazyVim](https://github.com/LazyVim/LazyVim).
+> Refer to the [documentation](https://lazyvim.github.io/installation) to get started.
+
+## Plugins
+
+### LSP client
+- clangd: C/C++
+- pylint: python
+
+
+### Search
+[Telescope](https://github.com/nvim-telescope/telescope.nvim)
+
+
+### Symbols outline
+[symbols-outline.nvim](https://github.com/simrat39/symbols-outline.nvim)
+
+
+### Completion engine
+[nvim-cmp](https://github.com/hrsh7th/nvim-cmp/)
+
+
+## TODO
+- [distant.nvim](https://github.com/chipsenkbeil/distant.nvim)
---
+++ lua/plugins/plugins.lua
@@ -0,0 +1,146 @@
+return {
+
+  -- add symbols-outline
+  {
+    "simrat39/symbols-outline.nvim",
+    cmd = "SymbolsOutline",
+    keys = { { "<leader>cs", "<cmd>SymbolsOutline<cr>", desc = "Symbols Outline" } },
+    config = true,
+  },
+
+  -- add more treesitter parsers
+  {
+    "nvim-treesitter/nvim-treesitter",
+    prefer_git = true,
+  },
+  -- add more treesitter parsers
+  {
+
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "bash",
+        "html",
+        "javascript",
+        "json",
+        "lua",
+        "markdown",
+        "markdown_inline",
+        "python",
+        "query",
+        "regex",
+        "tsx",
+        "typescript",
+        "vim",
+        "yaml",
+      },
+    },
+  },
+
+  -- add pyright to lspconfig
+  {
+    "neovim/nvim-lspconfig",
+    ---@class PluginLspOpts
+    opts = {
+      ---@type lspconfig.options
+      -- pyright will be automatically installed with mason and loaded with lspconfig
+    },
+    cervers = {
+      pyright = {},
+    },
+  },
+  {
+    "neovim/nvim-lspconfig",
+    ---@class PluginLspOpts
+    opts = {
+      ---@type lspconfig.options
+      cervers = {
+        clangd = {},
+      },
+    },
+  },
+
+  -- change some telescope options and a keymap to browse plugin files
+  {
+    "nvim-telescope/telescope.nvim",
+    keys = {
+      -- add a keymap to browse plugin files
+      -- stylua: ignore
+      {
+        "<leader>fp",
+        function() require("telescope.builtin").find_files({ cwd = require("lazy.core.config").options.root }) end,
+        desc = "Find Plugin File",
+      },
+    },
+    -- change some options
+    opts = {
+      defaults = {
+        layout_strategy = "horizontal",
+        layout_config = { prompt_position = "top" },
+        sorting_strategy = "ascending",
+        winblend = 0,
+      },
+    },
+
+    {
+      "hrsh7th/nvim-cmp",
+      version = false, -- last release is way too old
+      event = "InsertEnter",
+      dependencies = {
+        "hrsh7th/cmp-nvim-lsp",
+        "hrsh7th/cmp-buffer",
+        "hrsh7th/cmp-path",
+        "saadparwaiz1/cmp_luasnip",
+      },
+      opts = function()
+        vim.api.nvim_set_hl(0, "CmpGhostText", { link = "Comment", default = true })
+        local cmp = require("cmp")
+        local defaults = require("cmp.config.default")()
+        return {
+          completion = {
+            completeopt = "menu,menuone,noinsert",
+          },
+          snippet = {
+            expand = function(args)
+              require("luasnip").lsp_expand(args.body)
+            end,
+          },
+          mapping = cmp.mapping.preset.insert({
+            ["<C-n>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
+            ["<C-p>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
+            ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+            ["<C-f>"] = cmp.mapping.scroll_docs(4),
+            ["<C-Space>"] = cmp.mapping.complete(),
+            ["<C-y>"] = cmp.mapping.abort(),
+            ["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+            ["<S-CR>"] = cmp.mapping.confirm({
+              behavior = cmp.ConfirmBehavior.Replace,
+              select = true,
+            }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+          }),
+          sources = cmp.config.sources({
+            { name = "nvim_lsp" },
+            { name = "luasnip" },
+            { name = "buffer" },
+            { name = "path" },
+          }),
+          formatting = {
+            format = function(_, item)
+              local icons = require("lazyvim.config").icons.kinds
+              if icons[item.kind] then
+                item.kind = icons[item.kind] .. item.kind
+              end
+              return item
+            end,
+          },
+          experimental = {
+            ghost_text = {
+              hl_group = "CmpGhostText",
+            },
+          },
+          sorting = defaults.sorting,
+        }
+      end,
+    },
+  },
+}
